2a7a974b67e4ddd40d1b486ea91a4b704d59a392
Kiran B S <kbs71190@gmail.com>
2021-08-09 20:45:45 +0530

    Initial Commit
    Cloud Gateway

---
+++ cloud-gateway/src/test/java/com/luv2codeinjava/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2codeinjava.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
